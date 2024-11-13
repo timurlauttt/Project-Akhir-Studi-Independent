3e66edd033b21930c252c239681abc2c5d01ba1a
nofiiy <163719633+nofiiy@users.noreply.github.com>
2024-11-13 18:19:29 +0700

    Update forms.py

--- questify_project/questify_app/forms.py
+++ questify_project/questify_app/forms.py
@@ -1,6 +1,8 @@
 # forms.py
 from django import forms
 from .models import ContactMessage
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
     class Meta:
@@ -33,3 +35,11 @@ class ContactForm(forms.ModelForm):
                 'required': True
             }),
         }
+
+class CreateUserForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, help_text='Nama depan')
+    last_name = forms.CharField(max_length=30, required=True, help_text='Nama belakang')
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
